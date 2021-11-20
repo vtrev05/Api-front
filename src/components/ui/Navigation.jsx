0a81835dab269e7d02249809fd95396a4dafe79f
vtrev05 <lamary@gmail.comvtrev05@gmail.com>
2021-11-20 18:09:46 +0100

    body changes globalstyles try 6

--- src/components/ui/Navigation.jsx
+++ src/components/ui/Navigation.jsx
@@ -102,27 +102,11 @@ cursor: pointer;
 }
 `;
 
-
-const Body = styled.body`
-    margin: 0;
-    padding: 0;
-    @import url("https://candyfonts.com/wp-data/2018/10/26/11543/Formula1-Regular_web_0.ttf");
-    background: ${({ theme }) => theme.body};
-    color: ${({ theme }) => theme.text};
-    font-family: 'formula1-display-regular';
-    transition: all 0.50s linear;
-    a, i {
-      color: ${({ theme }) => theme.text}; 
-    }`
-
-
-
 const Navigation = () => {
 const [isOpen, setIsOpen] = useState(false);
 
     return (
         <HashRouter>
-            <Body>
             <Nav>
             <CheckBtn onClick={() =>
             setIsOpen(!isOpen)
@@ -172,7 +156,6 @@ const [isOpen, setIsOpen] = useState(false);
                 <Route path="/documentation" element={<Documentation />} />
                 <Route path="/about" element={<About />} />
             </Routes>
-            </Body>
         </HashRouter>
         
     )
