e8ae022610ff97b5170061f0eeae787c9586ed62
vtrev05 <lamary@gmail.comvtrev05@gmail.com>
2021-11-20 18:11:52 +0100

    body changes globalstyles try 6

--- src/App.js
+++ src/App.js
@@ -2,27 +2,15 @@ import React from 'react';
 import Header from './components/ui/Header';
 import Navigation from './components/ui/Navigation';
 import Footer from './components/ui/Footer';
-import styled from 'styled-components';
 
-const Body = styled.body`
-    margin: 0;
-    padding: 0;
-    @import url("https://candyfonts.com/wp-data/2018/10/26/11543/Formula1-Regular_web_0.ttf");
-    background: ${({ theme }) => theme.body};
-    color: ${({ theme }) => theme.text};
-    font-family: 'formula1-display-regular';
-    transition: all 0.50s linear;
-    a, i {
-      color: ${({ theme }) => theme.text}; 
-    }`
 
 function App() {
   return (
-    <Body>
+    <>
         <Header></Header>
         <Navigation></Navigation>
         <Footer></Footer>
-    </Body>
+    </>
   );
 }
 
